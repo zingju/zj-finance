c5ca184c2b10db79681d4f0e12463ec249545f9b
Zing-J <z3566608086@gmail.com>
2020-11-21 13:04:08 +0800

    新手特权组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,19 @@
   <div>
     <Heador></Heador>
     <Hslider></Hslider>
+    <Novice></Novice>
   </div>
 </template>
 
 <script>
 import Heador from "./Header";
 import Hslider from "./Hslider";
+import Novice from "./Novice";
 export default {
   components: {
     Heador,
     Hslider,
+    Novice,
   },
 };
 </script>
