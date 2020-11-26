76632f90b2e7e6b060bd1e65007e6939c0ae0154
Zing-J <z3566608086@gmail.com>
2020-11-26 12:38:59 +0800

    money组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     <Heador></Heador>
     <Hslider></Hslider>
     <Novice></Novice>
+    <Loan></Loan>
+    <Money></Money>
   </div>
 </template>
 
@@ -10,11 +12,15 @@
 import Heador from "./Header";
 import Hslider from "./Hslider";
 import Novice from "./Novice";
+import Loan from "./Loan";
+import Money from "./Money";
 export default {
   components: {
     Heador,
     Hslider,
     Novice,
+    Loan,
+    Money,
   },
 };
 </script>
